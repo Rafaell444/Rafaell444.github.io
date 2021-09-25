26125ca3fdff7d20c12b67276e9770facc7c7710
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2021-09-25 21:39:59 +0400

    prices changed

--- scripts/bff.js
+++ scripts/bff.js
@@ -4,9 +4,9 @@ const prodNames = [
     {id : "3", name :"40X50"},
     {id : "4", name :"50X70"},
     {id : "5", name :"60X80"},
-    {id : "6", name :"ტილო ან ფოტოქაღალდი"},
+    {id : "6", name :"მხოლოდ ტილო"},
     {id : "7", name :"ფოტოქაღალდი + პასპარტუ + ჩარჩო"},
-    {id : "8", name :"ტილო/ფოტოქაღალდი + ჩარჩო"},
+    {id : "8", name :"ფოტოქაღალდი + ჩარჩო"},
 
 
     
@@ -17,21 +17,21 @@ const prodNames = [
 
 
   const apiResult = [
-    {title:"24X30 BFF №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 25"},
-    {title:"30X42 BFF №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 35"},
-    {title:"40X50 BFF №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 45"},
-    {title:"50X70 BFF №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 60"},
-    {title:"60X80 BFF №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 80"},
+    {title:"24X30 BFF №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 25"},
+    {title:"30X42 BFF №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 35"},
+    {title:"40X50 BFF №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 45"},
+    {title:"50X70 BFF №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 60"},
+    {title:"60X80 BFF №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 80"},
     {title:"24X30 BFF №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 25"},
     {title:"30X42 BFF №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 35"},
     {title:"40X50 BFF №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 45"},
     {title:"50X70 BFF №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 60"},
     {title:"60X80 BFF №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 80"},
-    {title:"24X30 BFF №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 25"},
-    {title:"30X42 BFF №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 35"},
-    {title:"40X50 BFF №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 45"},
-    {title:"50X70 BFF №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 60"},
-    {title:"60X80 BFF №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 80"},
+    {title:"24X30 BFF №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 25"},
+    {title:"30X42 BFF №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 35"},
+    {title:"40X50 BFF №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 45"},
+    {title:"50X70 BFF №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 60"},
+    {title:"60X80 BFF №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 80"},
 
 ]
   
--- scripts/num.js
+++ scripts/num.js
@@ -4,9 +4,9 @@ const prodNames = [
     {id : "3", name :"40X50"},
     {id : "4", name :"50X70"},
     {id : "5", name :"60X80"},
-    {id : "6", name :"ტილო ან ფოტოქაღალდი"},
+    {id : "6", name :"მხოლოდ ტილო"},
     {id : "7", name :"ფოტოქაღალდი + პასპარტუ + ჩარჩო"},
-    {id : "8", name :"ტილო/ფოტოქაღალდი + ჩარჩო"},
+    {id : "8", name :"ფოტოქაღალდი + ჩარჩო"},
 
   ]
 
@@ -14,21 +14,21 @@ const prodNames = [
 
   const apiResult = [
     
-    {title:"24X30 Anniversarry  №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 25"},
-    {title:"30X42 Anniversarry  №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 35"},
-    {title:"40X50 Anniversarry  №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 45"},
-    {title:"50X70 Anniversarry  №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 60"},
-    {title:"60X80 Anniversarry  №1",description: "მასალა: ტილო ან ფოტოქაღალდი",output: "ფასი: 80"},
+    {title:"24X30 Anniversarry  №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 25"},
+    {title:"30X42 Anniversarry  №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 35"},
+    {title:"40X50 Anniversarry  №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 45"},
+    {title:"50X70 Anniversarry  №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 60"},
+    {title:"60X80 Anniversarry  №1",description: "მასალა: მხოლოდ ტილო",output: "ფასი: 80"},
     {title:"24X30 Anniversarry  №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 25"},
     {title:"30X42 Anniversarry  №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 35"},
     {title:"40X50 Anniversarry  №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 45"},
     {title:"50X70 Anniversarry  №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 60"},
     {title:"60X80 Anniversarry №2",description: "მასალა: ფოტოქაღალდი + პასპარტუ + ჩარჩო",output: "ფასი: 80"},
-    {title:"24X30 Anniversarry  №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 25"},
-    {title:"30X42 Anniversarry  №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 35"},
-    {title:"40X50 Anniversarry  №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 45"},
-    {title:"50X70 Anniversarry  №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 60"},
-    {title:"60X80 Anniversarry  №3",description: "მასალა: ტილო/ფოტოქაღალდი + ჩარჩო",output: "ფასი: 80"},
+    {title:"24X30 Anniversarry  №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 25"},
+    {title:"30X42 Anniversarry  №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 35"},
+    {title:"40X50 Anniversarry  №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 45"},
+    {title:"50X70 Anniversarry  №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 60"},
+    {title:"60X80 Anniversarry  №3",description: "მასალა: ფოტოქაღალდი + ჩარჩო",output: "ფასი: 80"},
 
   ];
   
