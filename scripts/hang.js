01ea572e7557e7e6dfe43090c3ed01bce3212f44
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2021-10-04 12:11:08 +0400

    hangcollage

---
+++ scripts/hang.js
@@ -0,0 +1,15 @@
+const prodNames = [
+    {id : "1", name :"30X40"},
+    {id : "2", name :"60X40"},
+
+  ]
+
+
+
+  const apiResult = [
+    {title:"სამაგრებიანი №1 30X40",description: "ჩარჩოს ფერი: ⚫ ⚪ 🔴 🟡",description1: "ფოტოების რაოდენობა: 6 ცალი",output: "ფასი: 30"},
+    {title:"სამაგრებიანი №2 30X40",description: "ჩარჩოს ფერი: ⚫ ⚪ 🔴 🟡",description1: "ფოტოების რაოდენობა: 6 ცალი",output: "ფასი: 30"},
+    {title:"სამაგრებიანი №3 60X40",description: "ჩარჩოს ფერი: ⚫ ⚪ 🔴 🟡",description1: "ფოტოების რაოდენობა: 12 ცალი",output: "ფასი: 40"},
+    {title:"სამაგრებიანი №4 60X40",description: "ჩარჩოს ფერი: ⚫ ⚪ 🔴 🟡",description1: "ფოტოების რაოდენობა: 12 ცალი",output: "ფასი: 40"},
+];
+
